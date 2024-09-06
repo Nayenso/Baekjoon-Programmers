445189eb2e505b9adc230d16a73cc98d1088f6c2
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-06 20:30:36 +0900

    [Bronze V] Title: A+B - 8, Time: 216 ms, Memory: 18912 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int T = input.nextInt();
+        int A, B;
+
+        for (int i = 1; i <= T; i++){
+            A = input.nextInt();
+            B = input.nextInt();
+            print(A,B,i);
+        }
+    }
+
+    static void print(int a, int b, int i){
+        System.out.println("Case #" +i+ ": " +a+ " + " +b+ " = "+(a+b));
+    }
+}
