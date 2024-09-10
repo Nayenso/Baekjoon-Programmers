26e58f7cba99a0b8e8f28c68716a0111083fd641
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-10 16:18:31 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 180 ms, Memory: 17900 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        while(input.hasNext()){
+            String word = input.nextLine();
+            System.out.println(word);
+        }
+    }
+}
