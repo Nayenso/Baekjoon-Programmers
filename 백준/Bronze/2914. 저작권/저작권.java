400fceeae9e27ac962caaae74a397038c4416d74
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-15 23:31:30 +0900

    [Bronze III] Title: 저작권, Time: 168 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int A = input.nextInt();
+        int I = input.nextInt();
+
+        System.out.println((A*(I-1)+1));
+    }
+
+}
