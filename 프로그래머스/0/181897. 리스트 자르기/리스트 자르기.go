dfc467f896409ba71b1166424ee0f0008bbc6c81
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-21 16:16:52 +0900

    [level 0] Title: 리스트 자르기, Time: 0.00 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.go
@@ -0,0 +1,17 @@
+func solution(n int, slicer []int, num_list []int) []int {
+    switch n {
+    case 1:
+        return num_list[:slicer[1]+1]
+    case 2:
+        return num_list[slicer[0]:]
+    case 3:
+        return num_list[slicer[0] : slicer[1]+1]
+    case 4:
+        result := []int{}
+        for i := slicer[0]; i <= slicer[1]; i += slicer[2] {
+            result = append(result, num_list[i])
+        }
+        return result
+    }
+    return []int{}
+}
